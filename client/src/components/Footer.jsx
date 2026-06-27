ffb59248ea9893f5fadad8a8fa3d380549845777
Sanket Mistry <sanketmistry.codes@gmail.com>
2026-06-27 14:56:21 +0530

    ui: use transparent colored logo in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -37,7 +37,7 @@ const Footer = () => {
         <div className="flex flex-col gap-4">
           <div className="flex items-center">
             <img
-              src="/FLORINAA_Logo_Upscaled.png"
+              src="/FLORINAA_Logo_Transparent.png"
               alt="Florinaa Logo"
               className="h-10 w-auto object-contain"
             />
